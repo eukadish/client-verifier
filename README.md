d570628d2f7a074e284d21ea75da6f3f604f7216
eukadish <eugene.kadish@gmail.com>
2023-12-31 09:37:46 -0500

    add reference text for check

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# client-verifier
+# Vlient Verifier
+
+A self contained `node` app that converts an HTML file into its representative
+DOM and checks the script tags.
+
+```
+    node main.js
+```
--- main.js
+++ main.js
@@ -17,21 +17,32 @@ const content = `
                    
             </script>
             <script>
-                var cheese = 'blargus';
+                const text = 'blargus';
+
+                console.log(text.concat('4t'));
             </script>
         </body>
     </html>
 `;
 
+const reference = `
+const name = 'Fred';
+
+console.log(name);
+`;
+
 const $ = cheerio.load(content);
 
-function strip(test) {
-    return _.trim(_.replace(_.replace(_.replace(target, /(\r\n|\n|\r)/g, ''), ';', '; '), /\s+/g, ' '));
+const strip = (target) => {
+    const parsedLineBreaks = _.replace(target, /(\r\n|\n|\r)/g, '');
+    const parsedCommas = _.replace(parsedLineBreaks, ';', '; ');
+    const parsedWhiteSpace = _.replace(parsedCommas, /\s+/g, ' ');
+
+    return _.trim(parsedWhiteSpace);
 }
 
-async function digest(encoded) {
+const digest = async (encoded) => {
     const hashBuffer = await crypto.subtle.digest('SHA-256', encoded);
-
     const hashArray = Array.from(new Uint8Array(hashBuffer));
 
     const hashHex = hashArray
@@ -42,36 +53,9 @@ async function digest(encoded) {
 }
 
 const target = $('script:eq(0)').text();
-// const stripped = _.replace(_.replace(target, /(\r\n|\n|\r)/g, ''), /\s+/g, ' ');
-// const stripped = _.replace(_.replace(_.replace(target, /(\r\n|\n|\r)/g, ''), ';', '; '), /\s+/g, ' ');
-
-// console.log(_.trim(stripped));
-
-// const encoded = (new TextEncoder()).encode(stripped);
-
-const reference = `
-const name = 'Fred';
-
-console.log(name);
-`;
-
-// TODO: replace the semicolons first
-
-// console.log(_.trim(_.replace(_.replace(_.replace(reference, /(\r\n|\n|\r)/g, ''), /\s+/g, ' '), ';', '; ')));
-
-// TODO: replace semicolon with space semicolon
-
-// console.log(` * hash: ${hmac.digest()}`);
-
-// const hash = await crypto.subtle.digest("SHA-256", data);
-
-// crypto.subtle.digest("SHA-256", encoded).then((hash) => console.log(hash.toString('hex')) );
 
 const strippedTarget = strip(target);
 const strippedReference = strip(reference);
-
-console.log(strippedTarget);
-console.log(strippedReference);
 
 Promise.all([
     digest((new TextEncoder()).encode(strippedTarget)),
@@ -79,6 +63,7 @@ Promise.all([
 ]).then((res) => {
     console.log(res[0]);
     console.log(res[1]);
-})
 
-// verify(encoded);
+    console.log(_.isEqual(res[0], res[1]));
+    console.log(_.isEqual($('body').find('script').length, 2));
+});
